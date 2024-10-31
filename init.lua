56c0a80bbbd9844806b6075d034240faad5e21dc
Nicholas Allen <nick@Nicks-MacBook-Pro.local>
2024-10-31 17:46:11 +0100

    Color scheme

--- init.lua
+++ init.lua
@@ -230,7 +230,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-
+  'Mofiqul/vscode.nvim',
+  'tomasiser/vim-code-dark',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
@@ -844,7 +845,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'codedark'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'

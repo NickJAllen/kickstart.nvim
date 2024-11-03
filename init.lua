69208f1946a66188476a8c868b5857ed33c95d03
Nicholas Allen <nick@MacBookPro.fritz.box>
2024-11-03 14:06:56 +0100

    Use Lspsaga plugin

--- init.lua
+++ init.lua
@@ -233,6 +233,16 @@ require('lazy').setup({
   'Mofiqul/vscode.nvim',
   'tomasiser/vim-code-dark',
   'mfussenegger/nvim-jdtls',
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require('lspsaga').setup {}
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter', -- optional
+      'nvim-tree/nvim-web-devicons', -- optional
+    },
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
